f97f40de374809bdd14ef78ae082c0923e387dc7
jericho-millena <21-03025@g.batstate-u.edu.ph>
2024-05-07 14:39:58 +0800

    12th

--- src/app/components/Card2.js
+++ src/app/components/Card2.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Card2 = ({ user }) => {
   // Determine the color based on progress
   const progressBarColor = user.progress >= 50 ? "bg-red-500" : "bg-gray-200";
 
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-lg p-4 flex flex-col sm:flex-row items-center">
+    <div className="flex flex-col sm:flex-row items-center bg-white rounded-lg overflow-hidden shadow-lg p-4">
       {/* Progress Circle Bar on Left */}
       <div className="flex-none mr-4">
         <div className="w-20 h-20 relative">
           {/* Gray circle */}
           <svg className="absolute" width="100%" height="100%">
-            <circle cx="50%" cy="50%" r="40%" fill="none" stroke="gray" strokeWidth="4" />
+            <circle
+              cx="50%"
+              cy="50%"
+              r="40%"
+              fill="none"
+              stroke="gray"
+              strokeWidth="4"
+            />
           </svg>
           {/* Red progress arc */}
-          <svg className="absolute" width="100%" height="100%" transform="rotate(-90)">
-            <circle cx="50%" cy="50%" r="40%" fill="none" stroke="red" strokeWidth="4" 
-                    strokeDasharray={`${user.progress * 2.513}, 251.3`} />
+          <svg
+            className="absolute"
+            width="100%"
+            height="100%"
+            transform="rotate(-90)"
+          >
+            <circle
+              cx="50%"
+              cy="50%"
+              r="40%"
+              fill="none"
+              stroke="red"
+              strokeWidth="4"
+              strokeDasharray={`${user.progress * 2.513}, 251.3`}
+            />
           </svg>
           {/* Progress text */}
           <span className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-red-500">
@@ -24,7 +43,7 @@ const Card2 = ({ user }) => {
           </span>
         </div>
       </div>
-      
+
       {/* Research Title, Author, Research Info, and SDG Data */}
       <div className="flex-grow">
         <div className="text-left mb-2">
@@ -38,7 +57,13 @@ const Card2 = ({ user }) => {
           <div className="sdg-list">
             <ul className="flex flex-wrap">
               {user.sdg.map((goal, index) => (
-                <li key={index} className="text-black-600 bg-red-500 rounded-lg px-2 py-1 mb-1 border border-black-300 text-sm" style={{ whiteSpace: 'nowrap' }}>{goal}</li>
+                <li
+                  key={index}
+                  className="text-black-600 bg-red-500 rounded-lg px-2 py-1 mb-1 border border-black-300 text-sm"
+                  style={{ whiteSpace: "nowrap" }}
+                >
+                  {goal}
+                </li>
               ))}
             </ul>
           </div>
