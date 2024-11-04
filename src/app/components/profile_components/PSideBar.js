c26b9b491551e329ac56fdde5da7fe95789997b3
jericho-millena <millenajerichojames@gmail.com>
2024-11-04 15:14:00 +0800

    Add ProfileMain and ProfileList components; remove Profile2 and Card components

---
+++ src/app/components/profile_components/PSideBar.js
@@ -0,0 +1,95 @@
+// components/PSideBar.js
+
+import React from "react";
+
+export default function PSideBar() {
+  return (
+    <aside
+      id="logo-sidebar"
+      className="fixed top-30 left-0 z-40 w-80 h-screen transition-transform -translate-x-full sm:translate-x-0" // Changed width from w-64 to w-80
+      aria-label="Sidebar"
+    >
+      <div className="h-full p-6 overflow-y-auto bg-gray-300">
+        <img src="/RProfile.png" className="h-20 me-10 pb-2 sm:h-100" />
+        <span className="flex items-center py-2 text-xl text-gray-900 rounded-lg">
+          Firstname Middlename Lastname
+        </span>
+        <span className="flex items-center py-2 text-sm text-slate-800 rounded-lg">
+          Doctorate Degree in Computer Science{" "}
+        </span>
+        <ul>
+          <li className="py-1">
+            <div className="flex items-center text-gray-900 rounded-lg dark:text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-8"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 2.25a.75.75 0 0 0 0 1.5v16.5h-.75a.75.75 0 0 0 0 1.5H15v-18a.75.75 0 0 0 0-1.5H3ZM6.75 19.5v-2.25a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-.75.75h-3a.75.75 0 0 1-.75-.75ZM6 6.75A.75.75 0 0 1 6.75 6h.75a.75.75 0 0 1 0 1.5h-.75A.75.75 0 0 1 6 6.75ZM6.75 9a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75ZM6 12.75a.75.75 0 0 1 .75-.75h.75a.75.75 0 0 1 0 1.5h-.75a.75.75 0 0 1-.75-.75ZM10.5 6a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75Zm-.75 3.75A.75.75 0 0 1 10.5 9h.75a.75.75 0 0 1 0 1.5h-.75a.75.75 0 0 1-.75-.75ZM10.5 12a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75ZM16.5 6.75v15h5.25a.75.75 0 0 0 0-1.5H21v-12a.75.75 0 0 0 0-1.5h-4.5Zm1.5 4.5a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Zm.75 2.25a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75v-.008a.75.75 0 0 0-.75-.75h-.008ZM18 17.25a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+
+              <span className="text-xs px-2">
+                Knowledge, Innovation, and Science Technology(KIST) Park
+              </span>
+            </div>
+          </li>
+          <li className="py-1">
+            <div className="flex items-center text-gray-900 rounded-lg dark:text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-7"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                />
+              </svg>
+
+              <span className="text-xs px-2">www.website.com</span>
+            </div>
+          </li>
+          <li className="py-1">
+            <div className="flex items-center text-gray-900 rounded-lg dark:text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+              >
+                <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
+              </svg>
+              <span className="text-xs px-2">www.linkedin.com/john-doe</span>
+            </div>
+          </li>
+          <li className="py-1">
+            <div className="flex items-center text-gray-00 rounded-lg dark:text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+              >
+                <path d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z"></path>
+              </svg>
+              <span className="text-xs px-2">www.facebook.com/john-doe</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+}
---
+++ src/app/pages/profile/ProfileMain/page.js
@@ -0,0 +1,17 @@
+// pages/Profile2.js
+
+import React from "react";
+import PSideBar from "@/app/components/profile_components/PSideBar";
+
+const ProfileMain = () => {
+  return (
+    <div>
+      <PSideBar />
+      <div className="ml-80">
+        <h1>Profile Page Content Here</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileMain;
