d510f18e8d17b1fc590326ac282eda687f2cca74
Joshua John De Guzman <21-09333@g.batstate-u.edu.ph>
2024-11-18 12:45:25 +0800

    Add a MapPin

--- src/app/components/papers_component/Authorsdata.js
+++ src/app/components/papers_component/Authorsdata.js
@@ -1,27 +1,31 @@
 "use client";
 
 import React from "react";
+import { ChevronDown, MapPin } from "lucide-react";
 
 const AuthorsData = ({ authors }) => {
   return (
-    <div className="mt-4 grid grid-cols-2 gap-4"> {/* Keep gap for spacing */}
+    <div className="mt-4 grid grid-cols-2 gap-4"> 
       {authors.map((author, index) => (
         <a
           key={index}
-          href="/pages/profile/ProfileMain" // Directly link to ProfileMain
+          href="/pages/profile/ProfileMain" 
           className="bg-gray-100 p-4 rounded-lg shadow-md flex items-center w-full max-w-full hover:bg-gray-200 transition-colors" // Add hover effect
         >
           <img
             src={author.image}
             alt="profile"
-            width={90} // Keep the smaller image size
-            height={60} // Keep the smaller image size
-            className="rounded-full" // Circular image
+            width={90} 
+            height={60} 
+            className="rounded-full" 
           />
-          <div className="ml-4 flex flex-col w-full"> {/* Stretch the text content */}
+          <div className="ml-4 flex flex-col w-full"> 
             <h3 className="text-sm font-semibold text-gray-900">{author.name}</h3>
             <p className="text-gray-600 text-xs">{author.degree}</p>
-            <p className="text-gray-600 text-xs mt-1">{author.university}</p>
+            <div className="flex items-center text-gray-600 text-xs mt-1"> 
+              <MapPin className="w-4 h-4 mr-1" /> 
+              {author.university}
+            </div>
           </div>
         </a>
       ))}
