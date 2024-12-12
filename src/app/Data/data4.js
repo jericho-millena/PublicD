3e7ea014f45f2ea6ef2d4a9b666ce3d9401dd7d2
jericho-millena <millenajerichojames@gmail.com>
2024-12-12 21:04:07 +0800

    Update navbar item links to use absolute paths for consistency

--- src/app/Data/data4.js
+++ src/app/Data/data4.js
@@ -3,11 +3,11 @@
 export const data = [];
 
 export const navbarItems = [
-  { id: 1, label: "Home", link: "./" },
-  { id: 2, label: "Profile", link: "./pages/profile" },
-  { id: 3, label: "Projects", link: "./pages/projects" },
-  { id: 4, label: "Papers", link: "./pages/papers" },
-  { id: 6, label: "Centers", link: "./pages/centers" },
+  { id: 1, label: "Home", link: "/" },
+  { id: 2, label: "Profile", link: "/pages/profile" },
+  { id: 3, label: "Projects", link: "/pages/projects" },
+  { id: 4, label: "Papers", link: "/pages/papers" },
+  { id: 6, label: "Centers", link: "/pages/centers" },
 ];
 
 export const user = [
