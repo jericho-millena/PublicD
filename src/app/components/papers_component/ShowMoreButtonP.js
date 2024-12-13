94b416173ee4080ca8e98611e7182779f897a3a2
jericho-millena <millenajerichojames@gmail.com>
2024-12-13 10:02:30 +0800

    Enhance Show More button: improve styling and add arrow icons for toggle state

--- src/app/components/papers_component/ShowMoreButtonP.js
+++ src/app/components/papers_component/ShowMoreButtonP.js
@@ -42,9 +42,45 @@ const ShowMoreButtonP = ({ authors }) => {
       <div className="flex justify-center mt-4">
         <button
           onClick={handleToggle}
-          className="py-2 px-4 bg-red-500 text-white rounded hover:bg-blue-600 transition"
+          className="bg-red-600 text-white px-4 py-2 w-48 max-w-xs shadow-lg mt-4 focus:outline-none hover:bg-red-700 hover:text-white transition duration-300 flex items-center justify-center"
         >
-          {showMore ? "Show Less" : "Show More"}
+          <span className="mr-2">{showMore ? "Show Less" : "Show More"}</span>
+
+          {/* Arrow icon for showing more */}
+          {!showMore && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-4 w-4 ml-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 15.75 7.5-7.5 7.5 7.5"
+              />
+            </svg>
+          )}
+
+          {/* Arrow icon for showing less */}
+          {showMore && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-4 w-4 ml-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          )}
         </button>
       </div>
     </div>
