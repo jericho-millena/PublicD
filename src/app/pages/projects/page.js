ccb647040a2736ba64fc437718d5d7d2672dbc0d
jericho-millena <millenajerichojames@gmail.com>
2024-12-17 19:34:07 +0800

    Add a button for searchbar

--- src/app/pages/projects/page.js
+++ src/app/pages/projects/page.js
@@ -117,6 +117,26 @@ export default function Profile() {
             value={searchQuery}
             onChange={handleSearch}
           />
+          <button
+            type="submit"
+            className="absolute right-0 top-0 bottom-0 px-3 text-gray-600 hover:text-gray-700 focus:outline-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+            <span className="sr-only">Filter</span>
+          </button>
         </form>
       </nav>
 
